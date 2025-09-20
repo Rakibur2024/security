71d2479473678362a02583ea0482eccfa84a3209
Rakibur2024 <rrahmanemail@gmail.com>
2025-09-20 16:22:16 +0100

    Implemented JWT

--- src/main/java/com/spring/security/util/JWTUtil.java
+++ src/main/java/com/spring/security/util/JWTUtil.java
@@ -1,8 +1,10 @@
 package com.spring.security.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -12,7 +14,7 @@ import java.util.Date;
 public class JWTUtil {
     private final String SECRET = "my-super-secret-key-that-is-long-enough-123456789!@#";
     private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
-    private final long EXPIRATION_TIME = 1000*60*60;
+    private final long EXPIRATION_TIME = 1000*60*1;
 
     public String generateToken(String username){
         return Jwts.builder()
@@ -21,5 +23,27 @@ public class JWTUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public String extractUsername(String token){
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String username, UserDetails userDetails, String token) {
+        // check if username in db is same as username in userDetails
+        //check if token expired
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token) // get everyting from token
+                .getBody();
     }
 }
