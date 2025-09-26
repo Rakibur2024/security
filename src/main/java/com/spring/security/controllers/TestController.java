b79f25d012a7792c87094008f9e851ce2e7cf310
Rakibur2024 <rrahmanemail@gmail.com>
2025-09-26 20:04:56 +0600

    Implemented @PreAuthorize and @PostAuthorize

--- src/main/java/com/spring/security/controllers/TestController.java
+++ src/main/java/com/spring/security/controllers/TestController.java
@@ -1,5 +1,7 @@
 package com.spring.security.controllers;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +23,23 @@ public class TestController {
     }
 
     @PostMapping("/demoPost")
+    @PreAuthorize("hasAuthority('SECURITY_WRITE')")
     public String demoPost(){
         return "Demo Post";
+    }
+
+    @GetMapping("/test3")
+    @PreAuthorize("hasAuthority('SECURITY_READ')")
+    public String test3(){
+        System.out.println("Test Method 3");
+        return "Test 3";
+    }
+
+    @GetMapping("/postAuthTest")
+    @PostAuthorize("returnObject == 'Test 3' or hasAuthority('SECURITY_WRITE')")
+    public String postAuthTest() {
+        System.out.println("Inside postAuthTest method...");
+        return "Test 3"; // You can change this return value to test
     }
 }
 
